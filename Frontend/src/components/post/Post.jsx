b64371b5c66c3bc49948e586bad2c08abe6c8e5e
YUTA INAGAWA <obanndesu11241988@yahoo.co.jp>
2023-04-05 14:46:10 +0900

    画像表示不具合修正

--- Frontend/src/components/post/Post.jsx
+++ Frontend/src/components/post/Post.jsx
@@ -4,6 +4,7 @@ import "./Post.css";
 import { Users } from "../../dummyDate";
 
 export const Post = ({ post }) => {
+  const PUBLIC_FOLDER = process.env.REACT_APP_PUBLIC_FOLDER;
   const [like, setLike] = useState(post.like);
   const [isLiked, setIsLiked] = useState(false);
 
@@ -19,6 +20,7 @@ export const Post = ({ post }) => {
           <div className="postTopLeft">
             <img
               src={
+                PUBLIC_FOLDER +
                 Users.filter((user) => user.id === post.id)[0].profilePicture
               }
               alt=""
@@ -35,12 +37,12 @@ export const Post = ({ post }) => {
         </div>
         <div className="postCenter">
           <span className="postText">{post.desc}</span>
-          <img src={post.photo} alt="" className="postImg" />
+          <img src={PUBLIC_FOLDER + post.photo} alt="" className="postImg" />
         </div>
         <div className="postBottom">
           <div className="postBottomLeft">
             <img
-              src="./assets/heart.png"
+              src={PUBLIC_FOLDER + "/heart.png"}
               alt=""
               className="likeIcon"
               onClick={() => handleLike()}
--- Frontend/src/components/rightbar/Rightbar.jsx
+++ Frontend/src/components/rightbar/Rightbar.jsx
@@ -4,16 +4,17 @@ import { Users } from "../../dummyDate";
 import { Online } from "../online/Online";
 
 export const Rightbar = ({ profile }) => {
+  const PUBLIC_FOLDER = process.env.REACT_APP_PUBLIC_FOLDER;
   const HomeRightbar = () => {
     return (
       <>
         <div className="eventContainer">
-          <img src="assets/star.png" alt="" className="starImg" />
+          <img src={PUBLIC_FOLDER + "/star.png"} alt="" className="starImg" />
           <span className="eventText">
             <b>フォロワー限定</b>イベント開催中！
           </span>
         </div>
-        <img src="assets/event.jpeg" alt="" className="eventImg" />
+        <img src={PUBLIC_FOLDER + "/event.jpeg"} alt="" className="eventImg" />
         <h4 className="rightbarTitle">オンラインの友達</h4>
         <ul className="rightbarFriendList">
           {Users.map((user) => (
@@ -22,19 +23,19 @@ export const Rightbar = ({ profile }) => {
         </ul>
         <p className="promotionTitle">プロモーション広告</p>
         <img
-          src="assets/promotion/promotion1.jpeg"
+          src={PUBLIC_FOLDER + "/promotion/promotion1.jpeg"}
           alt=""
           className="rightbarPromotionImg"
         />
         <p className="promotionName">ショッピング</p>
         <img
-          src="assets/promotion/promotion2.jpeg"
+          src={PUBLIC_FOLDER + "/promotion/promotion2.jpeg"}
           alt=""
           className="rightbarPromotionImg"
         />
         <p className="promotionName">カーショップ</p>
         <img
-          src="assets/promotion/promotion3.jpeg"
+          src={PUBLIC_FOLDER + "/promotion/promotion3.jpeg"}
           alt=""
           className="rightbarPromotionImg"
         />
@@ -56,7 +57,7 @@ export const Rightbar = ({ profile }) => {
           <div className="rightbarFollowings">
             <div className="rightbarFollowing">
               <img
-                src="assets/person/1.jpeg"
+                src={PUBLIC_FOLDER + "/person/1.jpeg"}
                 alt=""
                 className="rightbarFollowingImg"
               />
@@ -64,7 +65,7 @@ export const Rightbar = ({ profile }) => {
             </div>
             <div className="rightbarFollowing">
               <img
-                src="assets/person/2.jpeg"
+                src={PUBLIC_FOLDER + "/person/2.jpeg"}
                 alt=""
                 className="rightbarFollowingImg"
               />
@@ -72,7 +73,7 @@ export const Rightbar = ({ profile }) => {
             </div>
             <div className="rightbarFollowing">
               <img
-                src="assets/person/3.jpeg"
+                src={PUBLIC_FOLDER + "/person/3.jpeg"}
                 alt=""
                 className="rightbarFollowingImg"
               />
@@ -80,7 +81,7 @@ export const Rightbar = ({ profile }) => {
             </div>
             <div className="rightbarFollowing">
               <img
-                src="assets/person/4.jpeg"
+                src={PUBLIC_FOLDER + "/person/4.jpeg"}
                 alt=""
                 className="rightbarFollowingImg"
               />
@@ -88,7 +89,7 @@ export const Rightbar = ({ profile }) => {
             </div>
             <div className="rightbarFollowing">
               <img
-                src="assets/person/5.jpeg"
+                src={PUBLIC_FOLDER + "/person/5.jpeg"}
                 alt=""
                 className="rightbarFollowingImg"
               />
